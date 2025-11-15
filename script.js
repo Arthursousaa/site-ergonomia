b8394d140f67c0f48bddbffe1647deb7cf2ec08b
Arthursousaa <163069998+Arthursousaa@users.noreply.github.com>
2025-11-15 20:24:40 -0300

    Remove avaliar and enviarFormulario functions

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
-function avaliar() {
-  const resultado = document.getElementById("resultado");
-  resultado.innerHTML = "<p>Obrigado por participar! Reavalie sua postura e ambiente de trabalho com frequência.</p>";
-}
 
-function enviarFormulario(event) {
-  event.preventDefault();
-  const nome = document.getElementById("nome").value;
-  const resposta = document.getElementById("respostaForm");
-  resposta.innerHTML = `Obrigado, ${nome}! Sua mensagem foi enviada com sucesso.`;
-}
